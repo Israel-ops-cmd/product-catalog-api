fc47651eb96db8223ef758530f478f208688aa0b
Israel-ops-cmd <israellipe2020@gmail.com>
2025-06-05 16:25:06 -0300

    feat: create user

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express'
+import userRouters from './src/routers/user.router.js'
 
 const app = express()
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World')
-})
+app.use(express.json())
+app.use(userRouters)
 
 app.listen(3001, () => {console.log("Server is runnig on port 3001")})
