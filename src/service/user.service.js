581a2f454bfd3b12f07c9fc6104d1c187b8db1ba
Israel-ops-cmd <israellipe2020@gmail.com>
2025-06-05 17:49:15 -0300

    feat: check for duplicate username and email

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,6 +1,10 @@
 import userRepository from '../repositories/user.respositories.js'
 
 async function createUserService (newUser) {
+    const foundUserUsername = await userRepository.findUserByUsername(newUser.username)
+    if(foundUserUsername) throw new Error('User already exists!')
+    const foundUserEmail = await userRepository.findUserByEmail(newUser.email)
+    if(foundUserEmail) throw new Error('User already exists!')
     const user = await userRepository.createUserRepository(newUser)
     return user
 }
