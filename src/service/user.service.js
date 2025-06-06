5f7ac1b01860e030df58559d41112f969a8ac23b
Israel-ops-cmd <israellipe2020@gmail.com>
2025-06-06 14:32:17 -0300

    feat: add route to get all users

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -2,16 +2,24 @@ import userRepository from '../repositories/user.repositories.js'
 import bcrypt from 'bcrypt'
 
 async function createUserService (newUser) {
-    const foundUserUsername = await userRepository.findUserByUsername(newUser.username)
+    const foundUserUsername = await userRepository.findUserByUsernameRepository(newUser.username)
     if(foundUserUsername) throw new Error('User already exists!')
-    const foundUserEmail = await userRepository.findUserByEmail(newUser.email)
+    const foundUserEmail = await userRepository.findUserByEmailRepository(newUser.email)
     if(foundUserEmail) throw new Error('User already exists!')
+    const foundUserId = await userRepository.findUserByIdRepository(newUser.id)
+    if(foundUserId) throw new Error('User already exists')
     const passHash = await bcrypt.hash(newUser.password, 10)
     const user = await userRepository.createUserRepository({ ...newUser, password: passHash})
     if(!user) throw new Error('Error creating user!')
     return user
 }
 
+async function findAllUserService () {
+    const users = await userRepository.findAllUserRepository()
+    return users
+}
+
 export default {
-    createUserService
+    createUserService,
+    findAllUserService
 }
